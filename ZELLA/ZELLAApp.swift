d5bab28d087aa6c282b73474d0bbdc9f79415667
Booya <booya.pannachai@gmail.com>
2025-11-05 22:05:45 +0700

    first commit

---
+++ ZELLA/ZELLAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZELLAApp.swift
+//  ZELLA
+//
+//  Created by Panachai Sulsaksakul on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ZELLAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
